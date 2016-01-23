30a06f9b2495afd49e8367c49d355f193542dffd
SGarvin <sebastien.garvin@gmail.com>
2016-01-23 16:22:02 -0500

    test d'imagerie

---
+++ glo/codeTest/main.py
@@ -0,0 +1,10 @@
+# import the necessary packages
+import numpy as np
+import cv2
+
+from TraitementImage import TraitementImage
+
+traitementImage = TraitementImage()
+traitementImage.initCarte()
+
+

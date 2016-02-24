8c6e599911c5f3207e5b55fc6fd9314c0ba2ec47
luigigougougaga <luigigougougaga@gmail.com>
2016-02-24 06:01:26 -0500

    Interface Basique terminée. IMPORTANT : J'ai fait un changement dans le powerpoint. J'ai rajouté un image, mais comme j'ai openOffice, j'ai sauvegardé en ppt et non en pptx si quelqu'un pouvait faire le changement... Merci!

--- glo/interface/main.py
+++ glo/interface/main.py
@@ -9,19 +9,38 @@ class Interface(QtGui.QWidget):
         self.setWindowTitle('Interface')
         btn = QtGui.QPushButton('DEMARAGE', self)
         btn.resize(120,46)
-        btn.move(50, 50)
-
+        btn.move(200, 200)
 
 
     def paintEvent(self, e):
         qp = QtGui.QPainter()
         qp.begin(self)
+
+
+        self.setUpLiveFeed(qp)
         self.setUpLiveVirtuel(qp)
         self.drawRectangles(qp)
         self.drawCircles(qp)
         self.drawPolygones(qp)
         self.drawTriangles(qp)
         qp.end()
+
+    def setUpLiveFeed(self, qp):
+        self.noir(qp)
+        qp.drawText(50, 400 + 50, QtCore.QString("Trajectoire :"))
+        qp.drawText(50, 450 + 50, QtCore.QString("Tension condensateur :"))
+        qp.drawText(50, 500 + 50, QtCore.QString("Position et Orientation du Robot :"))
+        qp.drawText(50, 550 + 50, QtCore.QString("Ile cible :"))
+        self.blanc_contour_bleu(qp)
+        qp.drawRect(260, 385 + 50, 120, 20)
+        qp.drawRect(260, 435 + 50, 80, 20)
+        qp.drawRect(260, 485 + 50, 300, 20)
+        qp.drawRect(260, 535 + 50, 100, 20)
+        self.noir(qp)
+        qp.drawText(275, 400 + 50, QtCore.QString("88.95''N  15.10''O "))
+        qp.drawText(275, 450 + 50, QtCore.QString("1.23539 V "))
+        qp.drawText(275, 500 + 50, QtCore.QString("0.8245m, 0.23421m     68.35''S  1.36''O "))
+        qp.drawText(275, 550 + 50, QtCore.QString("Ile A"))
 
     def setUpLiveVirtuel(self, qp):
         qp.drawPixmap(640, 0, QtGui.QPixmap("test_image7.png"),0, 90, 640, 480)
@@ -73,6 +92,15 @@ class Interface(QtGui.QWidget):
             QtCore.QPoint(x + 0, y + 18)
         ])
         qp.drawConvexPolygon(polygone)
+
+    def blanc_contour_bleu(self, qp):
+        qp.setBrush(QtGui.QColor(250, 250, 250, 250))
+        qp.setPen(QtGui.QColor(0, 0, 250))
+
+
+    def noir(self, qp):
+        qp.setBrush(QtGui.QColor(0, 0, 0, 250))
+        qp.setPen(QtGui.QColor(0, 0, 0))
 
     def orange(self, qp):
         qp.setBrush(QtGui.QColor(252, 100, 0, 250))

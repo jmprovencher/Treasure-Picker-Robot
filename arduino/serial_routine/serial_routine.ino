e9d53d5a752b9378ff8583a9f0d02cd39db7ce44
comagnan <comagnan@coveo.com>
2016-02-27 15:47:09 -0500

    DPF et plan de test fixés, routine mise à jour.

--- arduino/serial_routine/serial_routine.ino
+++ arduino/serial_routine/serial_routine.ino
@@ -20,15 +20,15 @@ double Setpoint[4] = {3000, 3000, 3000, 3000};
 double Input[4] = {3000, 3000, 3000, 3000};
 double Output[4];
 
-PID firstPID(&Input[0], &Output[0], &Setpoint[0], 1, 2, 0.2, REVERSE);
-PID secondPID(&Input[1], &Output[1], &Setpoint[1], 1, 2, 0.2, REVERSE);
-PID thirdPID(&Input[2], &Output[2], &Setpoint[2], 1, 2, 0.2, REVERSE);
-PID fourthPID(&Input[3], &Output[3], &Setpoint[3], 1, 2, 0.2, REVERSE);
+PID firstPID(&Input[0], &Output[0], &Setpoint[0], 0.00005, 0.25, 0, REVERSE);
+PID secondPID(&Input[1], &Output[1], &Setpoint[1], 0.000066, 0.30, 0, REVERSE);
+PID thirdPID(&Input[2], &Output[2], &Setpoint[2], 0.00005, 0.2, 0, REVERSE);
+PID fourthPID(&Input[3], &Output[3], &Setpoint[3], 0.00005, 0.2, 0, REVERSE);
 
 PID pidList[4] = {firstPID, secondPID, thirdPID, fourthPID};
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   for(int i = 0; i < 6; i++){
     pinMode(pinsDrive[i], OUTPUT);
@@ -46,7 +46,7 @@ void loop() {
   if(duration != 0){
     analogWrite(pinsDrive[4], spdWheels[0]);
     analogWrite(pinsDrive[5], spdWheels[1]);
-    for(int i = 0; i < duration; i++){
+    for(int i = 0; i < (duration*2); i++){
       for(int j = 0; j<4; j++){
         spdBuffer = pulseIn(pinsRead[j], HIGH, 3000);
         if(spdBuffer == 0){
@@ -54,17 +54,22 @@ void loop() {
         }
         Input[j] = (spdBuffer);
         pidList[j].Compute();
-        Serial.print(Input[j]);
-        Serial.print(" - ");
-        Serial.print(Output[j]);
-        Serial.print(";");
+        //Serial.print(Input[j]);
+        //Serial.print(" - ");
+        //Serial.print(Output[j]);
+        //Serial.print(";");
       }
-      Serial.println(".");
-      Serial.print(action);
+      //Serial.println(".");
+      //Serial.print(action);
       for(int j = 0; j<4; j++){
-        analogWrite(pinsDrive[j], Output[j]); 
+        if(Setpoint[j] != 3000){
+          analogWrite(pinsDrive[j], Output[j]);
+        }
+        else{
+          analogWrite(pinsDrive[j], 0);
+        }
       }
-      Serial.println(incomingByte - i, DEC);
+      //Serial.println(incomingByte - i, DEC);
       delay(20);
     }
     for(int i = 0; i<4; i++){
@@ -80,28 +85,29 @@ void loop() {
 void serialEvent(){
     // read the incoming byte:
     incomingByte = Serial.read();
+    Serial.write(incomingByte);
     
     if(!mode){
       duration = 0;
       if(incomingByte == 56){
         action = "Moving forward ";
-        Setpoint[0] = 600; Setpoint[1] = 600; Setpoint[2] = 3000; Setpoint[3] = 3000; spdWheels[0] = 255, spdWheels[1] = 0;
+        Setpoint[0] = 800; Setpoint[1] = 800; Setpoint[2] = 3000; Setpoint[3] = 3000; spdWheels[0] = 255, spdWheels[1] = 0;
       }
       else if(incomingByte == 50){
         action = "Moving backwards ";
-        Setpoint[0] = 600; Setpoint[1] = 600; Setpoint[2] = 3000; Setpoint[3] = 3000; spdWheels[0] = 0, spdWheels[1] = 255;
+        Setpoint[0] = 800; Setpoint[1] = 800; Setpoint[2] = 3000; Setpoint[3] = 3000; spdWheels[0] = 0, spdWheels[1] = 255;
       }
       else if(incomingByte == 52){
         action = "Turning left ";
-        Setpoint[2] = 600; Setpoint[3] = 600; Setpoint[0] = 3000; Setpoint[1] = 3000; spdWheels[0] = 255, spdWheels[1] = 0;
+        Setpoint[2] = 800; Setpoint[3] = 800; Setpoint[0] = 3000; Setpoint[1] = 3000; spdWheels[0] = 255, spdWheels[1] = 0;
       }
       else if(incomingByte == 54){
         action = "Turning right ";
-        Setpoint[2] = 600; Setpoint[3] = 600; Setpoint[0] = 3000; Setpoint[1] = 3000; spdWheels[0] = 0, spdWheels[1] = 255;
+        Setpoint[2] = 800; Setpoint[3] = 800; Setpoint[0] = 3000; Setpoint[1] = 3000; spdWheels[0] = 0, spdWheels[1] = 255;
       }
       else{
         action = "Invalid action ";        
-        Setpoint[2] = 3000; Setpoint[3] = 3000; Setpoint[0] = 3000; Setpoint[1] = 3000; spdWheels[0] = 255, spdWheels[1] = 0;
+        Setpoint[2] = 800; Setpoint[3] = 3000; Setpoint[0] = 3000; Setpoint[1] = 800; spdWheels[0] = 255, spdWheels[1] = 0;
       }
       mode = true;
     }

180dbc9a18ac487051766ff0c39b370f59f80429
Jean-Michel Provencher <jeanmichelprovencher@hotmail.com>
2016-03-09 11:25:06 -0500

    quick fix for linux UART com port

--- glo/codeReconnaissanceFigures/robot/communication/mainRobot.py
+++ glo/codeReconnaissanceFigures/robot/communication/mainRobot.py
@@ -1,11 +1,15 @@
 from UARTDriver import UARTDriver
 from TCPClient import TCPClient
 monClient = TCPClient()
-#monUart = UARTDriver('COM8', 9600)
+# monUart = UARTDriver('COM8', 9600) #on Windows
+monUart = UARTDriver('dev/ttyACM0', 9600) #on linux
 
 while 1:
         data = monClient.receiveFile()
-        print data
-        commande = data['commande']
-        parametre = data['parametre']
-    #monUart.sendCommand(commande, parametre)
+        if data == -1:
+            print('Error while receiving file')
+        else:
+            print data
+            commande = data['commande']
+            parametre = data['parametre']
+            monUart.sendCommand(commande, parametre)

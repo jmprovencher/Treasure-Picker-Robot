a07bdfffb864200a03759445fb757147288539c8
SGarvin <sebastien.garvin@gmail.com>
2016-03-21 13:45:32 -0400

    nouvelle detection robot

--- glo/BlackPearl/stationbase/vision/DetectionRobot.py
+++ glo/BlackPearl/stationbase/vision/DetectionRobot.py
@@ -2,8 +2,7 @@
 import cv2
 import numpy as np
 import ConfigPath
-
-
+'''
 class DetectionRobot(object):
     def __init__(self, image):
         self.imageCamera = image
@@ -38,20 +37,20 @@ class DetectionRobot(object):
     def _detecterFormeCouleur(self, intervalleCouleur):
 
         intervalleFonce, intervalleClair, couleurForme = intervalleCouleur
-        masqueCouleur = cv2.inRange(self.imageCamera, intervalleFonce, intervalleClair)
-        _, contoursCouleur, _ = cv2.findContours(masqueCouleur.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        masqueRouge = cv2.inRange(self.imageCamera, intervalleFonce, intervalleClair)
+        _, contoursRouge, _ = cv2.findContours(masqueRouge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contoursNegligeable = []
 
-        for contours in range(len(contoursCouleur)):
-            aire = cv2.contourArea(contoursCouleur[contours])
+        for contours in range(len(contoursRouge)):
+            aire = cv2.contourArea(contoursRouge[contours])
             if ((aire < 1000) or (aire > 6000)):
                 contoursNegligeable.append(contours)
 
         if (len(contoursNegligeable) > 0):
-            contoursCouleur = np.delete(contoursCouleur, contoursNegligeable)
+            contoursRouge = np.delete(contoursRouge, contoursNegligeable)
         #a checker avec table si ca plante
-        if len(contoursCouleur) < 10:
-            for contoursForme in contoursCouleur:
+        if len(contoursRouge) < 10:
+            for contoursForme in contoursRouge:
                 self._trouverForme(contoursForme, couleurForme)
 
     def _definirIntervallesCouleurs(self):
@@ -65,6 +64,78 @@ class DetectionRobot(object):
         self.cntRobot = contoursRobot[0]
 
         self.formesConnues.append(self.cntRobot)
+
+'''
+class DetectionRobot(object):
+    def __init__(self, image):
+        self.imageCamera = image
+        self.formeAvant = None
+        self.formeArriere = None
+        self._definirIntervalleRouge()
+        self._definirPatronsFormes()
+        self.robotIdentifiee = None
+
+    def detecter(self):
+        self._detecterForme()
+
+    def _trouverForme(self, contours):
+
+        resultatsMatch = []
+        resultatsMatch.append((cv2.matchShapes(contours, self.cntTriangle, 1, 0.0), contours, 'Avant'))
+        resultatsMatch.append((cv2.matchShapes(contours, self.cntCercle, 1, 0.0), contours, 'Arriere'))
+
+        meilleurMatch = min(resultatsMatch)
+        precision, contours, nomForme = meilleurMatch
+        formeIdentifiee = contours, nomForme
+
+        if (precision < 0.5):
+            #print (nomForme, precision)
+            if (nomForme == 'Avant'):
+                self.formeAvant = formeIdentifiee
+            else:
+                self.formeArriere = formeIdentifiee
+
+        if ((self.formeArriere is None) or (self.formeAvant is None)):
+            self.robotIdentifiee = None
+        else:
+            self.robotIdentifiee = (self.formeAvant[0], self.formeArriere[0])
+
+    def _detecterForme(self):
+        intervalleFonce, intervalleClair, couleurForme = self.intervalleRouge
+        masqueRouge = cv2.inRange(self.imageCamera, intervalleFonce, intervalleClair)
+        _, contoursRouge, _ = cv2.findContours(masqueRouge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contoursNegligeable = []
+
+        for contours in range(len(contoursRouge)):
+            aire = cv2.contourArea(contoursRouge[contours])
+            if ((aire < 100) or (aire > 3000)):
+                contoursNegligeable.append(contours)
+
+        if (len(contoursNegligeable) > 0):
+            contoursRouge = np.delete(contoursRouge, contoursNegligeable)
+
+        for contoursForme in contoursRouge:
+            self._trouverForme(contoursForme)
+
+    def _definirIntervalleRouge(self):
+        self.intervalleRouge = np.array([15, 0, 75]), np.array([100, 65, 200]),'Rouge'
+
+    def _definirPatronsFormes(self):
+        patronRobotAvant = cv2.imread(ConfigPath.Config().appendToProjectPath('images/robotAvantPasEleve.png'), 0)
+        _, threshRobotAvant = cv2.threshold(patronRobotAvant, 127, 255, 0)
+        _, contoursRobotAvant, _ = cv2.findContours(threshRobotAvant, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.cntRobotAvant = contoursRobotAvant[0]
+        self.formesConnues.append(self.cntRobotAvant)
+
+        patronRobotArriere = cv2.imread(ConfigPath.Config().appendToProjectPath('images/robotArrierePasEleve.png'), 0)
+        _, threshRobotArriere = cv2.threshold(patronRobotArriere, 127, 255, 0)
+        _, contoursRobotArriere, _ = cv2.findContours(threshRobotArriere, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.cntRobotArriere = contoursRobotArriere[0]
+        self.formesConnues.append(self.cntRobotArriere)
+
+
+
+
 
 
 

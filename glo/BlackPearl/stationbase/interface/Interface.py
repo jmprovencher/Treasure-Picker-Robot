089307f3a45b7c6ebc97462b81e989c609fbf96d
luigigougougaga <luigigougougaga@gmail.com>
2016-03-23 19:45:39 -0400

    Affiche l'orientation du robot.

--- glo/BlackPearl/stationbase/interface/Interface.py
+++ glo/BlackPearl/stationbase/interface/Interface.py
@@ -9,9 +9,12 @@ from PyQt4.QtGui import QDialog
 from PyQt4.QtGui import QHeaderView
 from PyQt4.QtGui import QPushButton
 from PyQt4.QtGui import QLabel
+from PyQt4.QtGui import QPainter
+
 import sys
 import ConfigPath
 from stationbase.interface.StationBase import StationBase
+from stationbase.interface.AffichageDeBase import AffichageDeBase
 from stationbase.interface.AfficherImageVirtuelle import AfficherImageVirtuelle
 import time
 
@@ -21,19 +24,25 @@ class Interface(QtGui.QWidget):
         self.threadAfficherImageVirtuelle = AfficherImageVirtuelle(self)
         self.initUI()
 
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.affichageDeBase = AffichageDeBase(qp)
+        qp.end()
+
+
     def initUI(self):
         self.setWindowTitle('Interface')
         self.resize(1600, 1000)
         self.setAutoFillBackground(False)
-
         self.feed = QLabel(self)
         self.feed.setGeometry(0, 145, 1600, 855)
         self.feed.setPixmap(self.threadAfficherImageVirtuelle.imageConvertie)
-
+        self.orientation = QLabel(self)
+        self.orientation.setGeometry(380, 22, 440, 50)
         self.btnDemarer = QPushButton(self)
         self.btnDemarer.setText('Demarer')
         self.btnDemarer.setGeometry(40, 70, 98, 27)
-
         self.btnDemarer.clicked.connect(self.demarerRoutine)
 
     def demarerRoutine(self):
@@ -42,10 +51,14 @@ class Interface(QtGui.QWidget):
         self.connect(self.threadAfficherImageVirtuelle, QtCore.SIGNAL("update()"), self.update_gui)
         self.threadAfficherImageVirtuelle.start()
 
+
     def update_gui(self):
         self.feed.setPixmap(self.threadAfficherImageVirtuelle.imageConvertie)
         QtGui.QApplication.processEvents()
+        if(not self.threadStationBase.carte.infoRobot is None):
+            self.orientation.setText(QtCore.QString((str(self.threadStationBase.carte.infoRobot.orientation))+'\xb0'))
         self.feed.repaint()
+        self.orientation.repaint()
 
 
 

56b8988c8ab5f3498ba292874a9ebcde122bbfee
SGarvin <sebastien.garvin@gmail.com>
2016-03-24 18:37:42 -0400

    refactore

--- glo/BlackPearl/stationbase/vision/DetectionRobot.py
+++ glo/BlackPearl/stationbase/vision/DetectionRobot.py
@@ -6,7 +6,6 @@ import ConfigPath
 class DetectionRobot(object):
     def __init__(self, image):
         self.imageCamera = image
-        self.formesConnues = []
         self.formeAvant = None
         self.formeArriere = None
         self.conteurAvant = 0
@@ -69,13 +68,12 @@ class DetectionRobot(object):
         _, threshRobotAvant = cv2.threshold(patronRobotAvant, 127, 255, 0)
         _, contoursRobotAvant, _ = cv2.findContours(threshRobotAvant, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         self.cntRobotAvant = contoursRobotAvant[0]
-        self.formesConnues.append(self.cntRobotAvant)
 
         patronRobotArriere = cv2.imread(ConfigPath.Config().appendToProjectPath('images/etoilePattern.png'), 0)
         _, threshRobotArriere = cv2.threshold(patronRobotArriere, 127, 255, 0)
         _, contoursRobotArriere, _ = cv2.findContours(threshRobotArriere, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         self.cntRobotArriere = contoursRobotArriere[0]
-        self.formesConnues.append(self.cntRobotArriere)
+
 
 
 

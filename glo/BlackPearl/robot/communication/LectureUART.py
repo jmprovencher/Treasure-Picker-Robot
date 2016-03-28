3594eb9747b36cc7fbed2763401e565b88c8038b
jmprovencher <jeanmichelprovencher@hotmail.com>
2016-03-28 13:13:18 -0400

    lecture Uart modifie

--- glo/BlackPearl/robot/communication/LectureUART.py
+++ glo/BlackPearl/robot/communication/LectureUART.py
@@ -8,10 +8,14 @@ class LectureUART(Thread):
     def __init__(self, robot):
         Thread.__init__(self)
         self.robot = robot
+	print "Charlo Suck a linit"
 
     def run(self):
         while 1:
+	    print "Charlo suck"
             info = self.robot.uartDriver.UART.read(4)
+	    print "Lecture UART: "
+	    print info
             if (info == 'done'):
                 self.robot.commandeTerminee = True
             else:

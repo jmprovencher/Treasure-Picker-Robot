6b1e04b655269c9e13cf8dc58c6c87830e8bf142
SGarvin <sebastien.garvin@gmail.com>
2016-03-29 02:27:27 -0400

    amelioration de l'interface

--- glo/BlackPearl/stationbase/interface/RedirigeurTexte.py
+++ glo/BlackPearl/stationbase/interface/RedirigeurTexte.py
@@ -7,8 +7,7 @@ class RedirigeurTexte():
         self.tag = tag
 
     def write(self, str):
-        str = '\n' + str
-        str = textwrap.fill(str, self.widget.frameGeometry().width()/8)
+        #str = textwrap.fill(str, self.widget.frameGeometry().width()/8)
         if self.tag == "stdout":
             self.widget.setTextColor(QtCore.Qt.green)
         elif self.tag == "stderr":
@@ -17,3 +16,4 @@ class RedirigeurTexte():
         c = self.widget.textCursor()
         c.movePosition(QtGui.QTextCursor.End)
         self.widget.setTextCursor(c)
+        QtGui.QApplication.processEvents()

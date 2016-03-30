fb3ae2fc2478dd45f99ed7474ae88b6e9b8d6bad
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-03-29 20:22:39 -0400

    m

--- glo/BlackPearl/robot/communication/UARTDriver.py
+++ glo/BlackPearl/robot/communication/UARTDriver.py
@@ -22,6 +22,7 @@ class UARTDriver:
 
     def descendrePrehenseur(self):
         self.UART.write(b'P'.encode())
+        print("ALLO")
 
     def monterPrehenseur(self):
         self.UART.write(b'Q'.encode())

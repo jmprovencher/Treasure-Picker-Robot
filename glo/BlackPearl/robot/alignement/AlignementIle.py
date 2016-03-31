9826b246aba74b6084efa14ff6d1516d1aa123b3
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-03-31 01:02:33 -0400

    update

--- glo/BlackPearl/robot/alignement/AlignementIle.py
+++ glo/BlackPearl/robot/alignement/AlignementIle.py
@@ -6,12 +6,12 @@ class AlignementIle():
     def calculerAjustement(self, distance_x, distance_y):
         ajustements_x = self._ajusterPositionX(distance_x)
         ajustements_y = self._ajusterPositionY(distance_y)
-        if (ajustements_x > 0 ):
-            self.ajustements.append(ajustements_x)
-        if (ajustements_y > 0):
+
+        if (distance_x > 0):
+            self.ajustements.append()
+        if (distance_y > 0):
             self.ajustements.append(ajustements_y)
         return self.ajustements
-
 
     def _ajusterPositionX(self, distance_x):
         if (distance_x > 0):
@@ -19,12 +19,12 @@ class AlignementIle():
         elif (distance_x < 0):
             commande = 'right'
         print("Ajustement en X PIXEL: %d" % distance_x)
-        return commande, abs(distance_x/40)
+        return commande, abs(distance_x / 40)
 
     def _ajusterPositionY(self, distance_y):
         if (distance_y < 0):
             commande = 'backward'
         elif (distance_y > 0):
             commande = 'forward'
-        print("Ajustement en Y PIXEL: %d" %distance_y)
-        return commande, distance_y/40
+        print("Ajustement en Y PIXEL: %d" % distance_y)
+        return commande, distance_y / 40

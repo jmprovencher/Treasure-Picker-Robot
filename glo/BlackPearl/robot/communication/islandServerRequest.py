dea8b5b1528f1c81121d1bf45bf1b1800dbe412d
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-03-31 03:13:10 -0400

    update

--- glo/BlackPearl/robot/communication/islandServerRequest.py
+++ glo/BlackPearl/robot/communication/islandServerRequest.py
@@ -2,7 +2,8 @@ import urllib2
 import ssl
 
 def islandServerRequest(ipAdress, manchesterCode):
+    reseau = '132.203.14.228'
     gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)  # Only for gangstars
-    content = urllib2.urlopen("https://"+ipAdress+"/?code="+manchesterCode, context=gcontext).read()
+    content = urllib2.urlopen("https://"+reseau+"/?code="+manchesterCode, context=gcontext).read()
     return content
 

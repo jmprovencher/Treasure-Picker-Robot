ccfb1fe01ae124addaae9f2cc81495af48d3a918
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-04-05 00:24:26 -0400

    Modif print pour robot

--- glo/BlackPearl/robot/alignement/AlignementStation.py
+++ glo/BlackPearl/robot/alignement/AlignementStation.py
@@ -7,7 +7,8 @@ class AlignementStation():
 
     def calculerAjustement(self, distance_x, distance_y):
         ajustements_x = self._ajusterPositionLaterale_CM(distance_x)
-        self.ajustements.append(ajustements_x)
+        if (ajustements_x[1] >= 1):
+            self.ajustements.append(ajustements_x)
         ajustements_y = self._ajusterPositionY(distance_y)
         self.ajustements.append(ajustements_y)
 

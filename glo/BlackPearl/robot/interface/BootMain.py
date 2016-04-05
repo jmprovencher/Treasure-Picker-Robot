0d38fafbb5ec965de40320c5dbf56db16ace7b3d
SGarvin <sebastien.garvin@gmail.com>
2016-04-04 21:05:24 -0400

    bug fixe

--- glo/BlackPearl/robot/interface/BootMain.py
+++ glo/BlackPearl/robot/interface/BootMain.py
@@ -1,7 +1,7 @@
 import sys
+sys.path.append('/home/design3/Desktop/design3/glo/BlackPearl')
 from robot.communication.UARTDriver import UARTDriver
 from robot.interface.Robot import Robot
-sys.path.append('/home/design3/Desktop/design3/glo/BlackPearl')
 
 
 def main():

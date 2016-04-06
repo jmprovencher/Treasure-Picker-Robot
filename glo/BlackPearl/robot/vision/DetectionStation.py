045731210de08e1eb4dc990f16cbe21b8892648c
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-04-05 20:13:10 -0400

    Modification client

--- glo/BlackPearl/robot/vision/DetectionStation.py
+++ glo/BlackPearl/robot/vision/DetectionStation.py
@@ -56,6 +56,9 @@ class DetectionStation(object):
 
         _, contoursCouleur, _ = cv2.findContours(masqueCouleur.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        cv2.imshow("image", self.imageCamera)
+        cv2.waitKey(0)
+
         if (len(contoursCouleur) > 0):
             contoursCible = self._obtenirFormeInteret(contoursCouleur)
             aire = cv2.contourArea(contoursCible)

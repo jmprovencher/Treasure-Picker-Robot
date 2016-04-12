e3fa0080eb3a24f4644e6b1fb16b56d9a0ea4195
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-04-12 03:11:39 -0400

    Position tresor plus de robustesse

--- glo/BlackPearl/robot/vision/DetectionTresor.py
+++ glo/BlackPearl/robot/vision/DetectionTresor.py
@@ -71,6 +71,10 @@ class DetectionTresor(object):
             if ((aire < 1000) or (aire > 9000)):
                 contoursNegligeable.append(contours)
 
+            _, position_y = self._trouverCentreForme(contours)
+            if (position_y > self.positionZone[1]):
+                contoursNegligeable.append(contours)
+
         if (len(contoursNegligeable) > 0):
             contoursCouleur = np.delete(contoursCouleur, contoursNegligeable)
         if (len(contoursCouleur) == 0):

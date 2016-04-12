16a953cb931f5e53554344172663b6cc0fa8fa2b
SGarvin <sebastien.garvin@gmail.com>
2016-04-12 04:24:04 -0400

    update

--- glo/BlackPearl/robot/vision/DetectionTresor.py
+++ glo/BlackPearl/robot/vision/DetectionTresor.py
@@ -60,7 +60,7 @@ class DetectionTresor(object):
         masqueCouleur = cv2.inRange(self.imageCamera, intervalleFonce, intervalleClair)
         #kernel = np.ones((5, 5), np.uint8)
         #closing = cv2.morphologyEx(masqueCouleur, cv2.MORPH_CLOSE, kernel)
-        _, contoursCouleur, _ = cv2.findContours(masqueCouleur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #_, contoursCouleur, _ = cv2.findContours(masqueCouleur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         cv2.imshow("Tresor", masqueCouleur)
         cv2.waitKey(0)

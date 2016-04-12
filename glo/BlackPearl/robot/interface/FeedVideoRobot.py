ac3f14bf3dbd29f8a3d4dba409604fbd55bbaf1e
SGarvin <sebastien.garvin@gmail.com>
2016-04-12 03:59:31 -0400

    update

--- glo/BlackPearl/robot/interface/FeedVideoRobot.py
+++ glo/BlackPearl/robot/interface/FeedVideoRobot.py
@@ -32,7 +32,7 @@ class FeedVideoRobot(Thread):
         while (self.connecter):
             #print ("Streaming...")
             _, self.imageCapture = self.video.read()
-            intervalleJaune = np.array([20, 90, 90]), np.array([80, 255, 255]), "Jaune"
+            intervalleJaune = np.array([0, 0, 0]), np.array([255, 255, 255]), "Jaune"
             intervalleFonce, intervalleClair, couleurForme = intervalleJaune
             masqueCouleur = cv2.inRange(self.imageCapture, intervalleFonce, intervalleClair)
             #kernel = np.ones((5, 5), np.uint8)

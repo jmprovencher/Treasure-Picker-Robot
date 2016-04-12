603470bfc7c870a62de25ce084e2cffdca09fa07
SGarvin <sebastien.garvin@gmail.com>
2016-04-12 04:14:40 -0400

    update

--- glo/BlackPearl/robot/vision/AnalyseImageEmbarquee.py
+++ glo/BlackPearl/robot/vision/AnalyseImageEmbarquee.py
@@ -96,7 +96,7 @@ class AnalyseImageEmbarquee(Thread):
         self.imageCamera = self.robot.threadVideo.getImageCapture()
         intervalleJaune = np.array([0, 0, 0]), np.array([255, 255, 255]), "Jaune"
         intervalleFonce, intervalleClair, couleurForme = intervalleJaune
-        masqueCouleur = cv2.inRange(self.imageCapture, intervalleFonce, intervalleClair)
+        masqueCouleur = cv2.inRange(self.imageCamera, intervalleFonce, intervalleClair)
         #kernel = np.ones((5, 5), np.uint8)
         #closing = cv2.morphologyEx(masqueCouleur, cv2.MORPH_CLOSE, kernel)
         #_, contoursCouleur, _ = cv2.findContours(masqueCouleur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)

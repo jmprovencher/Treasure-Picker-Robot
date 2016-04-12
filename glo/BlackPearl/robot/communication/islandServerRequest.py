1bbb6721da2c79fd8b0b884de8b12809f09ab770
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-04-12 00:19:53 -0400

    Correction ajustement ile.

--- glo/BlackPearl/robot/communication/islandServerRequest.py
+++ glo/BlackPearl/robot/communication/islandServerRequest.py
@@ -6,7 +6,9 @@ def islandServerRequest(adresseIP, codeManchester):
     reseau = adresseIP
     gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
     content = urllib2.urlopen("https://" + reseau + "/?code=" + codeManchester, context=gcontext).read()
+    print("Envoie requete")
     while (content is None):
         content = urllib2.urlopen("https://" + reseau + "/?code=" + codeManchester, context=gcontext).read()
         print("GET request to server...")
+
     return content

42d80582c357da7152ad0c167e9db6df57127549
Xavier Kedzierski <xavierkedzi@gmail.com>
2016-04-13 00:30:26 -0400

    Ajout sleep durant requete au serveur

--- glo/BlackPearl/robot/communication/islandServerRequest.py
+++ glo/BlackPearl/robot/communication/islandServerRequest.py
@@ -1,14 +1,15 @@
 import urllib2
 import ssl
+import time
+
 
 
 def islandServerRequest(adresseIP, codeManchester):
     reseau = adresseIP
     gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    time.sleep(1)
     content = urllib2.urlopen("https://" + reseau + "/?code=" + codeManchester, context=gcontext).read()
-    print("Envoie requete")
-    while (content is None):
-        content = urllib2.urlopen("https://" + reseau + "/?code=" + codeManchester, context=gcontext).read()
-        print("GET request to server...")
+    time.sleep(1)
+    print("GET request to server...")
 
     return content

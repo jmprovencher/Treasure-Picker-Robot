db6b1a1a5d78c7aa42a285fdad4d95de3eda0135
Xavier Kedzierski <xavier.kedzierski-elgstrand.1@ulaval.ca>
2016-04-17 17:39:58 -0400

    Update FeedVideoRobot.py

--- glo/BlackPearl/robot/interface/FeedVideoRobot.py
+++ glo/BlackPearl/robot/interface/FeedVideoRobot.py
@@ -16,9 +16,9 @@ class FeedVideoRobot(Thread):
             try:
                 self.video = cv2.VideoCapture(camera_index)
                 self.video.set(3, 1600)
+                self.video.set(4, 1200)
                 _, self.imageCapture = self.video.read()
                 self.connecter = True
-                print("Connecter a index %d" %camera_index)
                 break
             except Exception as e:
                 print('Mauvais index de camera...')
@@ -27,11 +27,7 @@ class FeedVideoRobot(Thread):
         while (self.connecter):
             _, self.imageCapture = self.video.read()
 
-    def afficherFeed(self):
-        cv2.imshow("Stream robot", self.imageCapture)
-
     def getImageCapture(self):
-        print("Image prise pour traitement...")
         return self.imageCapture
 
     def demarrerCapture(self):

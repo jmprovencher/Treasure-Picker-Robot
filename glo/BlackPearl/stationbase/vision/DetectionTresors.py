56b22588a0814265a768b1c967bed567bb716a24
luigigougougaga <luigigougougaga@gmail.com>
2016-04-17 12:58:16 -0400

    no message

--- glo/BlackPearl/stationbase/vision/DetectionTresors.py
+++ glo/BlackPearl/stationbase/vision/DetectionTresors.py
@@ -6,6 +6,18 @@ from elements.Tresor import Tresor
 LIMITE_INF_TABLE_1_Y = 45
 LIMITE_SUP_TABLE_1_Y = 750
 LIMITE_SUP_TABLE_1_X = 1342
+LIMITE_INF_TABLE_2_Y = 45
+LIMITE_SUP_TABLE_2_Y = 810
+LIMITE_SUP_TABLE_2_X = 1350
+LIMITE_INF_TABLE_3_Y = 90
+LIMITE_SUP_TABLE_3_Y = 810
+LIMITE_SUP_TABLE_3_X = 1327
+LIMITE_INF_TABLE_5_Y = 50
+LIMITE_SUP_TABLE_5_Y = 800
+LIMITE_SUP_TABLE_5_X = 1350
+LIMITE_INF_TABLE_6_Y = 100
+LIMITE_SUP_TABLE_6_Y = 75
+LIMITE_SUP_TABLE_6_X = 1345
 
 
 class DetectionTresors(Detection):
@@ -58,13 +70,13 @@ class DetectionTresors(Detection):
                 if (LIMITE_INF_TABLE_1_Y < y < LIMITE_SUP_TABLE_1_Y) or (x > LIMITE_SUP_TABLE_1_X):
                     tresorsImpossible.append(i)
             elif self.numeroTable == 2:
-                if (45 < y < 810) or (x > 1350):
+                if (LIMITE_INF_TABLE_2_Y < y < LIMITE_SUP_TABLE_2_Y) or (x > LIMITE_SUP_TABLE_2_X):
                     tresorsImpossible.append(i)
             elif self.numeroTable == 3:
-                if (90 < y < 810) or (y < 10) or (x > 1327):
+                if (LIMITE_INF_TABLE_3_Y < y < LIMITE_SUP_TABLE_3_Y) or (x > LIMITE_SUP_TABLE_3_X):
                     tresorsImpossible.append(i)
             elif self.numeroTable == 5:
-                if (50 < y < 800) or (x > 1350):
+                if (LIMITE_INF_TABLE_5_Y < y < LIMITE_SUP_TABLE_5_Y) or (x > LIMITE_SUP_TABLE_5_X):
                     tresorsImpossible.append(i)
             elif self.numeroTable == 6:
                 if (100 < y < 750) or (x > 1345):
